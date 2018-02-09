3076f7110a404366ebdecc07daaaceb7510dc042
Ray Li <ray.li.verified@gmail.com>
2018-02-08 20:15:06 -0600

    Background Creation #2
    *Create new Colored Triangles background. 
    *Remove Geometric Mountains background.
    *Update Build Tools v27.0.3.

--- app/src/main/java/stream/custombuttonapp/MainActivity.java
+++ app/src/main/java/stream/custombuttonapp/MainActivity.java
@@ -3,6 +3,7 @@ package stream.custombuttonapp;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import stream.custombutton.CustomButton;
@@ -21,25 +22,25 @@ public class MainActivity extends AppCompatActivity {
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Log.d("Button", "Clicked");
             }
         };
 
-        CustomButton btn1 = (CustomButton) findViewById(R.id.btn1);
+        CustomButton btn1 = findViewById(R.id.btn1);
         btn1.setOnClickListener(onClickListener);
 
-        CustomButton btn2 = (CustomButton) findViewById(R.id.btn2);
+        CustomButton btn2 = findViewById(R.id.btn2);
         btn2.setOnClickListener(onClickListener);
 
-        CustomButton btn3 = (CustomButton) findViewById(R.id.btn3);
+        CustomButton btn3 = findViewById(R.id.btn3);
         btn3.setOnClickListener(onClickListener);
 
-        CustomButton btn4 = (CustomButton) findViewById(R.id.btn4);
+        CustomButton btn4 = findViewById(R.id.btn4);
         btn4.setDrawableRight(R.drawable.icon_check_white);
         btn4.setOnClickListener(onClickListener);
 
         int iconSize = (int) Units.spToPx(mContext, 22);
-        CustomButton btn5 = (CustomButton) findViewById(R.id.btn5);
+        CustomButton btn5 = findViewById(R.id.btn5);
         btn5.setDrawableLeft(R.drawable.icon_close_white, iconSize, iconSize);
         btn5.setOnClickListener(onClickListener);
     }
